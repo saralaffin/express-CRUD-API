e4b33f149d846264fea87e697228278d85d26f74
Sara Laffin <sarala@umich.edu>
2020-03-04 00:12:11 -0500

    work on seeding

--- data/csvToJson.js
+++ data/csvToJson.js
@@ -1,3 +1,5 @@
+//need to npm install convert-csv-to-json before running this file, but it's not needed for the app. I removed the dependenciesfrom package.json after getting my json files. This is more of a reminder for how I got my data in the first place
+
 let csvToJson = require("convert-csv-to-json");
 
 let fileInputName = "data/2019.csv";
@@ -8,4 +10,4 @@ csvToJson
   .formatValueByType()
   .generateJsonFileFromCsv(fileInputName, fileOutputName);
 
-//also ran from command line: csvtojson data/most_topographically_prominent_summits.csv > data/summitdata.json
+//also ran from command line: csvtojson data/most_topographically_prominent_summits.csv > data/summitdata.json to get the trickier data formatted
