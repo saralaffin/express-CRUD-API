8bf248b425eeea08615f358991a6d27c86de1c1d
Sara Laffin <sarala@umich.edu>
2020-03-06 11:17:29 -0500

    setting up for heroku deployment

--- index.js
+++ index.js
@@ -6,4 +6,8 @@ app.use(parser.json());
 
 app.use("/", require("./lib/routes/index"));
 
-app.listen(3000, () => console.log("listening on port 3000"));
+app.set("port", process.env.PORT || 8080);
+
+app.listen(app.get("port"), () => {
+  console.log(`✅ PORT: ${app.get("port")} 🌟`);
+});
