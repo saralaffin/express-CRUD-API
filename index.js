4b4ca2837d61631960a554a7931591c273f9393f
Sara Laffin <sarala@umich.edu>
2020-03-06 12:28:30 -0500

    install cors

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require("express");
+var cors = require("cors");
 const app = express();
 const parser = require("body-parser");
 
+app.use(cors());
 app.use(parser.json());
 
 app.use("/", require("./lib/routes/index"));
