0bde6cfc3a0e618344f51472f583c74529b3d3cc
Sara Laffin <sarala@umich.edu>
2020-03-06 10:37:58 -0500

    pulled index file out to root directory

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ const parser = require("body-parser");
 
 app.use(parser.json());
 
-app.use("/", require("./routes/index"));
+app.use("/", require("./lib/routes/index"));
 
 app.listen(3000, () => console.log("listening on port 3000"));
